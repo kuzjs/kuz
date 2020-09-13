2b4f586ff2ac56cda3d120d03240a2ffc67edfec
Ankur Seth <madhumeya@gmail.com>
2020-09-13 06:49:46 +0530

    Now printing Header row

--- utils/table.js
+++ utils/table.js
@@ -22,7 +22,7 @@ Table.prototype.Clear = function () {
 }
 
 Table.prototype.AddColumn = function (columnName, columnLength) {
-	if (columnLength === undefined) {
+	if (columnLength === undefined || columnLength < columnName.length) {
 		columnLength = columnName.length;
 	}
 
@@ -78,20 +78,41 @@ Table.prototype.GetRowSeparator = function () {
 	return rowSeparator;
 }
 
-Table.prototype.PrintRow = function (rowIndex) {
-	let row = this.rowData[rowIndex];
-	let rowId = (rowIndex+1) + "";
+Table.prototype.GetRowString = function (rowId, row) {
 	let rowString = "| " + rowId.padStart(this.firstColumn.length) + " | ";
 	for (let j in row) {
 		let columnLength = this.GetColumnLength(j);
 		rowString += row[j].padStart(columnLength) + " | ";
 	}
+	return rowString;
+}
+
+Table.prototype.GetHeaderRowString = function () {
+	let row = [];
+	let rowId = this.firstColumn.name;
+	for (let i=0; i<this.columnObjects.length; i++) {
+		row.push(this.columnObjects[i].name);
+	}
+	return this.GetRowString(rowId, row);
+}
+
+Table.prototype.GetDataRowString = function (rowIndex) {
+	let row = this.rowData[rowIndex];
+	let rowId = (rowIndex+1) + "";
+	return this.GetRowString(rowId, row);
+}
+
+Table.prototype.PrintRow = function (rowIndex) {
+	let rowString = this.GetDataRowString(rowIndex);
 	console.log(rowString);
 }
 
 Table.prototype.Print = function () {
 	let rowSeparator = this.GetRowSeparator();
+	let headerRowString = this.GetHeaderRowString();
 
+	console.log(rowSeparator);
+	console.log(headerRowString);
 	console.log(rowSeparator);
 	for (let i=0; i<this.rowData.length; i++) {
 		this.PrintRow(i);
