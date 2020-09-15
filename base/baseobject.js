fc5b1f6d27c2909237931c97cb4104b8d2c94322
Ankur Seth <madhumeya@gmail.com>
2020-09-15 07:54:53 +0530

    Defined Triple funcs on Index

--- base/baseobject.js
+++ base/baseobject.js
@@ -13,6 +13,25 @@ KZBaseObject.prototype.Site = function (site) {
 	return this.site;
 }
 
+
+
+KZBaseObject.prototype.Index = function (index) {
+	this.SetIndex(index);
+	return this.GetIndex();
+}
+
+KZBaseObject.prototype.SetIndex = function (index) {
+	if (index != undefined) {
+		this.index = index;
+	}
+}
+
+KZBaseObject.prototype.GetIndex = function () {
+	return this.index;
+}
+
+
+
 KZBaseObject.prototype.CodeName = function () {
 	return this.codeLetter + this.index;
 }
