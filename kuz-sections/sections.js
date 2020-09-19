184da7be21e16c3c4c638d2403fe3e8306a0c54b
Ankur Seth <madhumeya@gmail.com>
2020-09-19 19:39:58 +0530

    Added GetLabel()

--- kuz-sections/sections.js
+++ kuz-sections/sections.js
@@ -1,11 +1,38 @@
 
 
 
-function KuzSections (sectionLines) {
+function GetLabel (sectionLine) {
+	let trimmedLine = sectionLine.trim();
+	if (trimmedLine.startsWith("[") && trimmedLine.endsWith("]")) {
+		let fullLabel = trimmedLine.slice(1, -1);
+		let labelParts = fullLabel.split("|");
+		let mods = [];
+		for (let labelPart of labelParts.slice(1)) {
+			mods.push(labelPart.trim());
+		}
+		return {
+			found: true,
+			name: labelParts[0].trim(),
+			mods: mods
+		};
+	} else {
+		return {
+			found: false
+		};
+	}
+}
+
+
+
+function Section (label) {
 	//
 }
 
 
+
+function KuzSections (sectionLines) {
+	//this.Setup(sectionLines);
+}
 
 KuzSections.prototype.IsValid = function () {
 	return true;
