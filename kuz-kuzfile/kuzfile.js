d4f6635aec74bbfe803db2b4ab098c0357208be4
Ankur Seth <madhumeya@gmail.com>
2020-09-24 09:57:40 +0530

    Added cache methods to KuzFile

--- kuz-kuzfile/kuzfile.js
+++ kuz-kuzfile/kuzfile.js
@@ -5,6 +5,7 @@ function KuzFile (owner, path) {
 	this.owner = owner;
 	this.path = path;
 	this.log = this.owner.log;
+	this.cache = false;
 
 	const KuzRegions = require("./kuz-regions").KuzRegions;
 	this.regions = new KuzRegions(this.path);
@@ -12,6 +13,28 @@ function KuzFile (owner, path) {
 	const KuzMetaData = require("./kuz-metadata").KuzMetaData;
 	this.metaData = new KuzMetaData(this, this.path);
 }
+
+
+
+KuzFile.prototype.turnCacheOn = function () {
+	this.cache = true;
+	return this;
+}
+
+KuzFile.prototype.turnCacheOff = function () {
+	this.cache = false;
+	return this;
+}
+
+KuzFile.prototype.cacheIsOn = function () {
+	return this.cache;
+}
+
+KuzFile.prototype.cacheIsOff = function () {
+	return !this.cacheIsOn();
+}
+
+
 
 KuzFile.prototype.getMetaData = function () {
 	return this.metaData;
